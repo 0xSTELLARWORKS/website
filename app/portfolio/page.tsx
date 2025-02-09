40543e2d2fd2e54f4500f9b1d509a9cbca0b0df1
Tanveer Najib <tanveernajib@gmail.com>
2025-02-09 11:30:37 +0000

    Allow links to be shared for specific tabs

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,30 +1,61 @@
 'use client'
 
-import { Page } from "@/components/Page";
-import Tankobon from "@/components/Portfolio/Tankobon";
-import Kaboom from "@/components/Portfolio/Kaboom";
-import { ScreenSelector } from "@/components/ScreenSelector/ScreenSelector";
-import { ScreenSelectorItem } from "@/components/ScreenSelector/ScreenSelectorItem";
-import { ScreenSelectorSideBar } from "@/components/ScreenSelector/ScreenSelectorSideBar";
-import { useState } from "react";
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { Page } from "@/components/Page"
+import Tankobon from "@/components/Portfolio/Tankobon"
+import Kaboom from "@/components/Portfolio/Kaboom"
+import { ScreenSelector } from "@/components/ScreenSelector/ScreenSelector"
+import { ScreenSelectorItem } from "@/components/ScreenSelector/ScreenSelectorItem"
+import { ScreenSelectorSideBar } from "@/components/ScreenSelector/ScreenSelectorSideBar"
+
+const SCREENS = ["kaboom", "tankobon"]
 
 export default function Portfolio() {
-  const [currentScreen, setCurrentScreen] = useState(0)
+  const router = useRouter()
+  const [screenName, setScreenName] = useState<string>("")
+
+  useEffect(() => {
+    const hash = window.location.hash.replace("#", "")
+
+    if (SCREENS.includes(hash)) {
+      setScreenName(hash)
+    } else {
+      const defaultScreen = SCREENS[0]
+      setScreenName(defaultScreen)
+      router.replace(`#${defaultScreen}`, {})
+    }
+  }, [router])
+
+  const updateHash = (newHash: string) => {
+    setScreenName(newHash)
+    router.replace(`#${newHash}`, {})
+  }
 
   const getScreen = () => {
     const screens = [
-      <Kaboom key={0} />,
-      <Tankobon key={1} />
+      <Kaboom key={0} />, <Tankobon key={1} />
     ]
-    return screens[currentScreen]
+    const index = SCREENS.indexOf(screenName)
+    return screens[index]
   }
 
   return (
     <Page>
       <ScreenSelector>
         <ScreenSelectorSideBar>
-          <ScreenSelectorItem name="Kaboom" image="/projects/kaboom.svg" active={currentScreen === 0} onClick={() => setCurrentScreen(0)}/>
-          <ScreenSelectorItem name="Tankobon" image="/projects/tankobon.svg" active={currentScreen === 1} onClick={() => setCurrentScreen(1)}/>
+          <ScreenSelectorItem
+            name="Kaboom"
+            image="/projects/kaboom.svg"
+            active={screenName === "kaboom"}
+            onClick={() => updateHash("kaboom")}
+          />
+          <ScreenSelectorItem
+            name="Tankobon"
+            image="/projects/tankobon.svg"
+            active={screenName === "tankobon"}
+            onClick={() => updateHash("tankobon")}
+          />
         </ScreenSelectorSideBar>
         {getScreen()}
       </ScreenSelector>
