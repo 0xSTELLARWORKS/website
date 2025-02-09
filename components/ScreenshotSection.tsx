52dff8d1af0c7648fac1d5f9d7a66901cdaa8b09
Tanveer Najib <tanveernajib@gmail.com>
2025-02-09 12:44:56 +0000

    Add screenshot viewer

--- components/ScreenshotSection.tsx
+++ components/ScreenshotSection.tsx
@@ -3,31 +3,39 @@
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Section } from "./Section"
 import Image from "next/image"
+import { ScreenshotViewer } from "./ScreenshotViewer"
+import { useState } from "react"
 
 interface Props {
   screenshots: string[]
 }
 
 export function ScreenshotSection({ screenshots }: Props) {
+  const [currentScreenshot, setCurrentScreenshot] = useState<number | undefined>(undefined)
+
   return (
-    <Section>
-      <h3 className="text-subheading font-extrabold tracking-wider">Screenshots:</h3>
-      <Swiper slidesPerView={'auto'} spaceBetween={16} freeMode mousewheel={{enabled: true}}>
-        {screenshots.map((image, index) => {
-          return (
-            <SwiperSlide key={index} style={{width: 'auto'}}>
-              <Image
-                className="w-fit h-[250px] border-[4px] border-accent"
-                src={image}
-                alt={`Kaboom screenshot ${index + 1}`}
-                height={0}
-                width={0}
-                unoptimized
-              />
-            </SwiperSlide>
-          )
-        })}
-      </Swiper>
-    </Section>
+    <>
+      <Section>
+        <h3 className="text-subheading font-extrabold tracking-wider">Screenshots:</h3>
+        <Swiper slidesPerView={'auto'} spaceBetween={16} freeMode mousewheel={{enabled: true}}>
+          {screenshots.map((image, index) => {
+            return (
+              <SwiperSlide key={index} style={{width: 'auto'}}>
+                <Image
+                  className="w-fit h-[250px] border-[4px] border-accent cursor-pointer"
+                  src={image}
+                  alt={`Kaboom screenshot ${index + 1}`}
+                  height={0}
+                  width={0}
+                  unoptimized
+                  onClick={() => setCurrentScreenshot(index)}
+                />
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </Section>
+      <ScreenshotViewer screenshots={screenshots} currentScreenshot={currentScreenshot} setCurrentScreenshotAction={setCurrentScreenshot} />
+    </>
   )
 }
---
+++ components/ScreenshotViewer.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { faCircleArrowLeft } from "@fortawesome/free-solid-svg-icons/faCircleArrowLeft"
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons/faCircleArrowRight"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Image from "next/image"
+import React from "react"
+
+interface Props {
+  screenshots: string[]
+  currentScreenshot: number | undefined
+  setCurrentScreenshotAction: React.Dispatch<React.SetStateAction<number | undefined>>
+}
+
+export function ScreenshotViewer({ screenshots, currentScreenshot, setCurrentScreenshotAction }: Props) {
+  const close = (event: React.MouseEvent) => {
+    if (event.currentTarget === event.target) {
+      setCurrentScreenshotAction(undefined)
+    }
+  }
+
+  const change = (offset: number) => {
+    if (currentScreenshot === undefined) { return }
+    let newScreenshot = currentScreenshot + offset
+    if (newScreenshot >= screenshots.length) {
+      newScreenshot = 0
+    }
+    if (newScreenshot < 0) {
+      newScreenshot = screenshots.length - 1
+    }
+    setCurrentScreenshotAction(newScreenshot)
+  }
+
+  return (
+    <>
+      {currentScreenshot !== undefined && (
+        <div className="z-20 fixed left-0 top-0 h-full w-full bg-black/50 backdrop-blur-sm" onClick={close}>
+          <Image
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-auto h-auto max-w-[80vw] max-h-[80vh] border-[4px] border-accent"
+            src={screenshots[currentScreenshot]}
+            alt={`Screenshot`}
+            height={0}
+            width={0}
+            unoptimized
+          />
+          <FontAwesomeIcon icon={faCircleArrowRight} className="text-4xl absolute right-0 top-1/2 -translate-y-1/2 mr-4 cursor-pointer" onClick={() => change(1)} />
+          <FontAwesomeIcon icon={faCircleArrowLeft} className="text-4xl absolute left-0 top-1/2 -translate-y-1/2 ml-4 cursor-pointer" onClick={() => change(-1)} />
+          <p className="absolute bottom-0 left-1/2 -translate-x-1/2 mb-8 font-extrabold text-2xl">{currentScreenshot + 1} / {screenshots.length}</p>
+        </div>
+      )}
+    </>
+  )
+}
