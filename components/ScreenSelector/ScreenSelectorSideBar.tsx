7182e2e377a9d82a5af071bf7ed1ea0fd2322664
Tanveer Najib <tanveernajib@gmail.com>
2025-02-09 11:40:42 +0000

    Fix some broken css

--- components/ScreenSelector/ScreenSelectorSideBar.tsx
+++ components/ScreenSelector/ScreenSelectorSideBar.tsx
@@ -7,7 +7,7 @@ interface Props {
 export function ScreenSelectorSideBar({ children }: Props) {
   return (
     <div className="pt-nav col-span-1 border-r-[4px] border-accent hidden md:block">
-      <div className="sticky top-sideBar transition-top duration-300 ease-in-out h-available overflow-y-auto">
+      <div className="sticky top-sideBar transition-top duration-300 ease-in-out h-available overflow-y-auto" style={{boxShadow: "0 -4px 0 rgb(var(--accent));"}}>
         {children}
       </div>
     </div>
