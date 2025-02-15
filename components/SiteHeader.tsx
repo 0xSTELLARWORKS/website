fff456dc4feb0c30be8e92ea8d27b5208cb2ad7e
Tanveer Najib <tanveernajib@gmail.com>
2025-02-15 14:13:50 +0000

    Try to make it obvious that nav scrolls

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -2,12 +2,16 @@
 
 import Image from "next/image"
 import { NavLink } from "./NavLink"
-import { useEffect, useState, useCallback } from "react"
+import { useEffect, useState, useCallback, useRef } from "react"
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub"
 import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram"
 
 export function SiteHeader() {
   const [isScrolled, setIsScrolled] = useState(false)
+
+  const navLinksRef = useRef<HTMLDivElement>(null)
+  const leftGradientRef = useRef<HTMLDivElement>(null)
+  const rightGradientRef = useRef<HTMLDivElement>(null)
 
   const handleScroll = useCallback(() => {
     if (window.innerWidth < 960) {
@@ -25,15 +29,36 @@ export function SiteHeader() {
     }
   }, [])
 
+  const handleNavLinksScroll = useCallback(() => {
+    if (navLinksRef.current && navLinksRef.current?.scrollLeft > 0) {
+      leftGradientRef.current?.classList.remove("opacity-0")
+    } else {
+      leftGradientRef.current?.classList.add("opacity-0")
+    }
+
+    // Do the same for the right gradient
+    if (navLinksRef.current && navLinksRef.current?.scrollLeft < navLinksRef.current.scrollWidth - navLinksRef.current.clientWidth) {
+      rightGradientRef.current?.classList.remove("opacity-0")
+    } else {
+      rightGradientRef.current?.classList.add("opacity-0")
+    }
+  }, [])
+
   useEffect(() => {
     handleScroll()
+    handleNavLinksScroll()
     window.addEventListener("scroll", handleScroll)
     window.addEventListener("resize", handleScroll)
+
+    const ref = navLinksRef
+    ref.current?.addEventListener("scroll", handleNavLinksScroll)
+
     return () => {
       window.removeEventListener("scroll", handleScroll)
       window.removeEventListener("resize", handleScroll)
+      ref.current?.removeEventListener("scroll", handleNavLinksScroll)
     }
-  }, [handleScroll])
+  }, [handleScroll, handleNavLinksScroll])
 
   return (
     <div
@@ -51,7 +76,8 @@ export function SiteHeader() {
           width={0}
           style={{ height: isScrolled ? "65px" : "170px" }}
         />
-        <div className="h-16 flex gap-4 justify-start items-center absolute bottom-0 w-full overflow-x-auto no-scrollbar">
+        <div className="h-16 flex gap-4 justify-start items-center absolute bottom-0 w-full overflow-x-auto no-scrollbar" ref={navLinksRef}>
+          <div className="opacity-0 pointer-events-none fixed h-[64px] sm:w-0 w-16 z-20 bg-gradient-to-r from-secondary/75 via-secondary/75 to-transparent transition duration-300 ease-in-out" ref={leftGradientRef} />
           <div className="flex gap-4 mx-auto px-4">
             <NavLink text="Home" href="/"/>
             <NavLink text="Portfolio" href="/portfolio"/>
@@ -60,6 +86,7 @@ export function SiteHeader() {
             <NavLink text="" href="https://github.com/0xSTELLARWORKS" icon={faGithub}/>
             <NavLink text="" href="https://github.com/0xSTELLARWORKS" icon={faInstagram}/>
           </div>
+          <div className="opacity-0 pointer-events-none fixed right-0 h-[64px] sm:w-0 w-16 z-20 bg-gradient-to-l from-secondary/75 via-secondary/75 to-transparent transition duration-300 ease-in-out" ref={rightGradientRef} />
         </div>
       </div>
     </div>
