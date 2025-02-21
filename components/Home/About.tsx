7b7a79331b0f1af62303929211db5315fd40ee1f
Tanveer Najib <tanveernajib@gmail.com>
2025-02-21 21:00:25 +0000

    Replace forest with simple stars

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -10,7 +10,7 @@ export default function About() {
         <h2 className="text-heading text-center relative home-title">Building stellar<br/>software!</h2>
       </Section>
 
-      <div className="w-full absolute bottom-0 h-[150px] bg-cover bg-center bg-no-repeat" style={{backgroundImage: 'url("/forest.svg")'}} />
+      <div className="w-full absolute bottom-0 h-[100px] bg-center bg-no-repeat" style={{backgroundImage: 'url("/stars.svg")'}} />
     </ScreenSelectorScreen>
   )
 }
