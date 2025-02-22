770c3b4cbef7de4523363fa4e01d9f98cefb7fb9
Tanveer Najib <tanveernajib@gmail.com>
2025-02-22 13:55:03 +0000

    Add macos style stop lights

--- components/HelloWorld.tsx
+++ components/HelloWorld.tsx
@@ -49,9 +49,16 @@ export function HelloWorld() {
   }, [index, isDeleting, statementIndex, code]);
 
   return (
-    <div className="flex flex-col gap-0">
-      <div className="bg-[#2E3440] px-2 py-1 text-sm w-fit rounded-t-[0.3em] font-mono">{language}</div>
-      <SyntaxHighlighter language={language} style={nord} showLineNumbers customStyle={{borderTopLeftRadius: 0, marginTop: 0}}>
+    <div className="flex flex-col gap-0 mt-8">
+      <div className="flex flex-row-reverse justify-between items-center bg-[#2E3440]/50 rounded-t-[0.3em] overflow-hidden h-8">
+        <div className="bg-[#2E3440] px-2 py-1 w-fit font-mono h-full">{language}</div>
+        <div className="flex gap-2 mx-2">
+          <div className="h-4 w-4 rounded-full bg-red-500" />
+          <div className="h-4 w-4 rounded-full bg-yellow-500" />
+          <div className="h-4 w-4 rounded-full bg-green-500" />
+        </div>
+      </div>
+      <SyntaxHighlighter language={language} style={nord} showLineNumbers customStyle={{borderTopLeftRadius: 0, borderTopRightRadius: 0, marginTop: 0}}>
         {text || "\u00A0"}
       </SyntaxHighlighter>
     </div>
